929f03c3cee54c4bc2832f1651cbb9a7e690e1ec
psonnera <patrick.sonnerat@hotmail.com>
2026-02-13 22:25:42 +0100

    Display rework
    Async chunked rendering

--- static/js/reportinit.js
+++ static/js/reportinit.js
@@ -1,47 +1,29 @@
 'use strict';
 
 $(document).ready(function() {
-			// Manual plugin initialization if not already done
-			if (!window.Nightscout) window.Nightscout = {};
-			if (!window.Nightscout.report_plugins) {
-				// Require plugin index and initialize with dummy context
-				// This assumes index.js is bundled and available as window.Nightscout.report_plugins_init
-				if (window.Nightscout.report_plugins_init) {
-					window.Nightscout.report_plugins = window.Nightscout.report_plugins_init({ language: 'en' });
-				}
-			}
-			if (window.Nightscout.report_plugins && typeof window.Nightscout.report_plugins.addHtmlFromPlugins === 'function') {
-				window.Nightscout.report_plugins.addHtmlFromPlugins(window.Nightscout.client || {});
-			}
-		// Handler for SHOW button
-		$('#rp_show').on('click', function() {
-			// Find selected plugin tab
-			var selectedTab = $('#tabnav > li.selected');
-			if (selectedTab.length === 0) {
-				selectedTab = $('#tabnav > li').first();
-			}
-			var pluginName = selectedTab.attr('id');
-			// Find plugin placeholder
-			var pluginPlaceholder = $('#' + pluginName + '-placeholder');
-			// Optionally, call report function for plugin
-			// This assumes plugins are registered globally in window.Nightscout.report_plugins
-			if (window.Nightscout && window.Nightscout.report_plugins && window.Nightscout.report_plugins[pluginName]) {
-				var plugin = window.Nightscout.report_plugins[pluginName];
-				if (typeof plugin.report === 'function') {
-					// Gather necessary arguments (datastorage, sorteddaystoshow, options)
-					// These should be set up elsewhere in your app
-					var datastorage = window.Nightscout.datastorage || {};
-					var sorteddaystoshow = window.Nightscout.sorteddaystoshow || [];
-					var options = window.Nightscout.options || {};
-					plugin.report(datastorage, sorteddaystoshow, options);
-				}
-			}
-			// Show plugin placeholder
-			$('.tabplaceholder').css('display', 'none');
-			pluginPlaceholder.css('display', '');
-		});
-	console.log('Application got ready event');
-	window.Nightscout.reportclient();
+	// Ensure window.Nightscout exists
+	if (!window.Nightscout) {
+		window.Nightscout = {};
+	}
+	
+	if (window.Nightscout.reportclient && typeof window.Nightscout.reportclient === 'function') {
+		window.Nightscout.reportclient();
+	}
+	
+	// Handler for SHOW button
+	$('#rp_show').on('click', function() {
+		// Find selected plugin tab
+		var selectedTab = $('#tabnav > li.selected');
+		if (selectedTab.length === 0) {
+			selectedTab = $('#tabnav > li').first();
+		}
+		var pluginName = selectedTab.attr('id');
+		// Find plugin placeholder
+		var pluginPlaceholder = $('#' + pluginName + '-placeholder');
+		// Show plugin placeholder
+		$('.tabplaceholder').css('display', 'none');
+		pluginPlaceholder.css('display', '');
+	});
 
 	// Ensure SHOW button is always enabled and visible
 	$('#rp_show').prop('disabled', false).show();
@@ -90,8 +72,7 @@ $(document).ready(function() {
 			agpTab.addClass('selected');
 			$('.tabplaceholder').css('display', 'none');
 			$('#glucosedistribution-placeholder').css('display', '');
+			$('#rp_show').prop('disabled', false).show().click();
 		}
-		$('#rp_show').prop('disabled', false).show().click();
 	}, 100);
-});
 });
