82a24e18b66b1041e2c1c75f63fc5abdb2f598d5
psonnera <patrick.sonnerat@hotmail.com>
2026-06-17 21:35:05 +0200

    Mongo 6 upgrade

--- testing/populate.js
+++ testing/populate.js
@@ -9,17 +9,16 @@ main();
 
 function main() {
   var MongoClient = mongodb.MongoClient;
-  MongoClient.connect(env.storageURI, {}, function connected(err, client) {
 
-    console.log('Connecting to mongo...');
-    if (err) {
-      console.log('Error occurred: ', err);
-      throw err;
-    }
+  console.log('Connecting to mongo...');
+  MongoClient.connect(env.storageURI, {}).then(function connected(client) {
 
     var db = client.db();
 
-    populate_collection(db);
+    return populate_collection(db);
+  }).catch(function (err) {
+    console.log('Error occurred: ', err);
+    throw err;
   });
 }
 
@@ -27,10 +26,7 @@ function populate_collection(db) {
   var cgm_collection = db.collection(env.entries_collection);
   var new_cgm_record = util.get_cgm_record();
 
-  cgm_collection.insertOne(new_cgm_record, function (err) {
-    if (err) {
-      throw err;
-    }
+  return cgm_collection.insertOne(new_cgm_record).then(function () {
     process.exit(0);
   });
 }
